2aba04882bf6747ae3aec9df220ff2fa72be0a8f
Justin <justinpxng@gmail.com>
2019-01-01 17:30:44 -0800

    Call BQ API and show result at an endpoint

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 import logging
+import numpy as np
 
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, render_template, request
+from google.cloud import bigquery
+from requests_toolbelt.adapters import appengine
+appengine.monkeypatch()
 
 app = Flask(__name__)
+
+
+def query_stackoverflow():
+    client = bigquery.Client()
+    query_job = client.query("""
+        SELECT
+          CONCAT(
+            'https://stackoverflow.com/questions/',
+            CAST(id as STRING)) as url,
+          view_count
+        FROM `bigquery-public-data.stackoverflow.posts_questions`
+        WHERE tags like '%google-bigquery%'
+        ORDER BY view_count DESC
+        LIMIT 10""")
+
+    results = query_job.result()  # Waits for job to complete.
+    results_dict = dict([ (row.url, row.view_count) for row in results ])
+    
+    return jsonify(results_dict)
+                
 
 @app.route('/form')
 def form():
@@ -22,3 +46,6 @@ def submitted_form():
         site=site,
         comments=comments)
 
+@app.route('/bq')
+def bq():
+    return query_stackoverflow()
