acc6e8ab9ab318bea54a313a796a38d4e82a9c66
Justin <justinpxng@gmail.com>
2019-01-03 22:11:33 -0800

    Pass user input to BQ query and render results

--- main.py
+++ main.py
@@ -9,43 +9,33 @@ appengine.monkeypatch()
 app = Flask(__name__)
 
 
-def query_stackoverflow():
+def query_stackoverflow(tag):
     client = bigquery.Client()
     query_job = client.query("""
         SELECT
-          CONCAT(
-            'https://stackoverflow.com/questions/',
-            CAST(id as STRING)) as url,
+          title,
           view_count
         FROM `bigquery-public-data.stackoverflow.posts_questions`
-        WHERE tags like '%google-bigquery%'
+        WHERE tags like '%""" + tag + """%'
         ORDER BY view_count DESC
         LIMIT 10""")
 
     results = query_job.result()  # Waits for job to complete.
-    results_dict = dict([ (row.url, row.view_count) for row in results ])
+    results_dict = dict([ (row.title, row.view_count) for row in results ])
     
-    return jsonify(results_dict)
+    return results_dict
                 
 
 @app.route('/form')
 def form():
     return render_template('form.html')
 
-@app.route('/submitted', methods=['POST'])
+@app.route('/submitted_form', methods=['POST'])
 def submitted_form():
-    name = request.form['name']
-    email = request.form['email']
-    site = request.form['site_url']
-    comments = request.form['comments']
-
+    tag = request.form['tag']
+    query_result = query_stackoverflow(tag)
+    
     return render_template(
         'submitted_form.html',
-        name=name,
-        email=email,
-        site=site,
-        comments=comments)
-
-@app.route('/bq')
-def bq():
-    return query_stackoverflow()
+        tag=tag,
+        titles=query_result)
